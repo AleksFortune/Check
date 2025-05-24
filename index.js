078f1bca0778f3d09b9547cf2551aaaf818a8be3
AleksFortune <alekspav65@gmail.com>
2025-05-25 01:39:32 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+export default function Home() {
+  return <div style={{textAlign: 'center', marginTop: '50px'}}>
+    <h1>Добро пожаловать в FortuneX!</h1>
+    <p>Это MVP версия сайта на Next.js</p>
+  </div>
+}
